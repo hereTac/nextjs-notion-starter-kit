815b83145142676146aac6a6c65fbdf4841e7647
xinz.zhang <xinz.zhang@shunwang.com>
2022-04-06 23:18:57 +0800

    增加Pages Iceberg 文件解析 V1

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 export default {
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '68011ade660b4599b74809601e96fd32',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
